71c8847bbb0119e241e58c8227681a4fc2c3a763
Sk.Muskan Fathima <124171813+SMILEYMUSKAN@users.noreply.github.com>
2023-09-10 09:22:50 +0000

    Home Page Css Added- UI

--- ecommerce/src/components/Home.js
+++ ecommerce/src/components/Home.js
@@ -1,10 +1,25 @@
+import { Link} from "react-router-dom";
+
 var Home = () => {
-    return (
-      <div>
-        <h1>Welcome to home page</h1>
+  return (
+    <section
+      className=" max-h-full block mx-auto mt-32 text-center "
+      id="section"
+    >
+      <div id="home">
+        <h1 className="text-5xl italic text-slate-950 ">
+          Welcome To Cart Fairy
+        </h1>
       </div>
-    );
-  };
-  
-  export default Home;
-  
+      <div>
+        <Link to="/products">
+          <button className="bg-slate-900 w-40 text-white block mx-auto text-lg italic mt-32 rounded mb-10 p-2">
+            View{" "}
+          </button>
+        </Link>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
