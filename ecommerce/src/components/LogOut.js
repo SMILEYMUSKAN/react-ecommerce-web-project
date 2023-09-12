844f442b1c7c2853414364365f044d3802fa26f5
Sk.Muskan Fathima <124171813+SMILEYMUSKAN@users.noreply.github.com>
2023-09-12 08:24:58 +0000

    Redirect to login page - LogOut

--- ecommerce/src/components/LogOut.js
+++ ecommerce/src/components/LogOut.js
@@ -1,13 +1,16 @@
 import { useEffect } from "react";
 import { useUserContext } from "../context/UserProvider";
+import { useHistory } from "react-router-dom";
 
 var LogOut = () => {
   var { LogOut } = useUserContext();
+  var history = useHistory()
   console.log(LogOut);
 
   useEffect(() => {
     LogOut();
     localStorage.clear()
+    history.push("/login")
   }, []);
   
   return null;
